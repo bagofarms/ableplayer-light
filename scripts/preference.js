821e36d19ae477e86f5b54bd0bcdf07dd5789bfa
Jacob Bates <jacob.bates@ucf.edu>
2016-07-12 14:04:03 -0400

    Removed JS cookie reliance to be compatible with including directly in Canvas custom JS

--- scripts/preference.js
+++ scripts/preference.js
@@ -1,6 +1,6 @@
 (function ($) {
   AblePlayer.prototype.setCookie = function(cookieValue) {
-    Cookies.set('Able-Player', cookieValue, { expires:90 });
+    //Cookies.set('Able-Player', cookieValue, { expires:90 });
     // set the cookie lifetime for 90 days
   };
 
@@ -12,21 +12,7 @@
       transcript: {}
     };
 
-    var cookie;
-    try {
-      cookie = Cookies.getJSON('Able-Player');
-    }
-    catch (err) {
-      // Original cookie can't be parsed; update to default
-      Cookies.getJSON(defaultCookie);
-      cookie = defaultCookie;
-    }
-    if (cookie) {
-      return cookie;
-    }
-    else {
-      return defaultCookie;
-    }
+    return defaultCookie;
   };
 
   AblePlayer.prototype.updateCookie = function( setting ) {
